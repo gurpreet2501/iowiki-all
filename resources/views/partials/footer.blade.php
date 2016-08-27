1caba5c98882996a3b1e7b2af84193e2ba0300c6
Gurpreet Singh <gurpreet@iowiki.com>
2016-08-27 01:03:17 -0700

    header footer added in code

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+	</body>
+</html>
+<script type="text/javascript" src="assets/js/jquery-3.1.0.min.js"></script>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="assets/css/wiki.css">
+	<title>Iowiki</title>
+</head>
+<body>
+<header>
+	<div class="container-fluid">
+		<div class="row">
+				<div class="col-md-12 text-center">
+					<div class="logo">
+							<img src="assets/images/logo.png" class="img-fit-container" />
+					</div>
+				</div>
+		</div>
+	</div>
+</header>
