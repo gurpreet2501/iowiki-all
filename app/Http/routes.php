e54ce3fbe9e21b3f64b0058da003ced160b20e76
Gurpreet Singh <gurpreet@iowiki.com>
2016-08-27 20:13:23 -0700

    header and ad section created

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('home/', 'HomeController@index');
+Route::get('/', 'HomeController@index');
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,18 +2,77 @@
 <html>
 <head>
  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="assets/css/font-awesome.min.css">
  <link rel="stylesheet" type="text/css" href="assets/css/wiki.css">
 	<title>Iowiki</title>
 </head>
 <body>
 <header>
-	<div class="container-fluid">
+	<div class="container">
 		<div class="row">
-				<div class="col-md-12 text-center">
+				<div class="col-md-2 text-center">
 					<div class="logo">
-							<img src="assets/images/logo.png" class="img-fit-container" />
+							<img src="assets/images/logo.png" width="75%" />
 					</div>
 				</div>
-		</div>
-	</div>
+				<div class="col-md-2"><i class="fa fa-car" aria-hidden="true"></i></div>
+				<div class="col-md-4"></div>
+				<div class="col-md-3">
+					<div class="search">
+						<form class="navbar-form navbar-left" id="search">
+			        <div class="form-group">
+			          <input type="text" class="form-control" placeholder="Search here">
+			        </div>
+		      	</form>
+					</div> <!-- search -->
+				</div> <!-- col-md-3 -->
+		</div> <!-- row -->
+	</div> <!-- container-fluid -->
 </header>
+<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="ad-code-768x90">
+						<img src="assets/images/ad.png" class="banner-ad" />
+				</div>
+				</div> <!-- col-md-12 -->
+		</div> <!-- row -->
+		<div class="row">
+				<div class="col-md-12">
+						<div class="categories text-center">
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+							<a class="btn btn-default category-btns" href="#" role="button">PHP</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Java</a>
+							<a class="btn btn-default category-btns" href="#" role="button">Android</a>
+							<a class="btn btn-default category-btns" href="#" role="button">ios</a>
+						</div>			
+				</div>
+		</div>
+</div>
